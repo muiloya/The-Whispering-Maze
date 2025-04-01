04928903a1533c6a8cf4e436e3da58897f740b75
muiloya <umairloya@yahoo.com>
2025-04-01 17:54:46 -0230

    Added vectorField for pathfinding

--- js/World/GameMap.js
+++ js/World/GameMap.js
@@ -46,10 +46,18 @@ export class GameMap {
     // Create our game object
     this.gameObject = this.mapRenderer.createRendering();
 
-    // Create our start & goal nodes. @TODO: needs refactoring for sure!!
+    // TODO: this section might need a refactor
+    // Create a random goal node
     this.goal = this.createRandomGoal();
-    this.costmap = this.mapGraph.singleGoalDijkstra(this.goal);
-    this.start = this.createRandomStart(this.goal, this.costmap, 20);
+
+    // create our costmap using dijkstra
+    this.costMap = this.mapGraph.singleGoalDijkstra(this.goal);
+
+    // create a random start node
+    this.start = this.createRandomStart(this.goal, this.costMap, 20);
+
+    // create our vector flow field for npc pathfinding
+    this.vectorField = this.setupVectorField(this.goal);
   }
 
   
@@ -92,6 +100,45 @@ export class GameMap {
         }
     }
     return startNode;
+  }
+
+  setupVectorField(goal) {
+    let vectorField = new Map();
+
+    // Compute movement vectors for each node
+    for (let node of this.mapGraph.nodes) {
+
+      // Skip obstacles and goal
+      if (!node.isTraversable() || node.id == goal.id) {
+        continue;
+      }
+  
+      let bestNeighbor = null;
+      let bestCost = Infinity;
+  
+      // Check all neighboring nodes to find the neighbor with the best cost
+      for (let edge of node.edges) {
+        let neighbor = edge.node;
+        let cost = this.costMap.get(neighbor);
+  
+        if (cost < bestCost) {
+          bestCost = cost;
+          bestNeighbor = neighbor;
+        }
+      }
+  
+      // Compute movement vector pointing to best neighbor, if no valid neighbour set zero vector.
+      if (bestNeighbor != null) {
+        let from = this.localize(node);
+        let to = this.localize(bestNeighbor);
+        let direction = new THREE.Vector3().subVectors(to, from).normalize();
+        vectorField.set(node, direction);
+      } else {
+        vectorField.set(node, new THREE.Vector3(0, 0, 0));
+      }
+    }    
+    
+    return vectorField;
   }
 
 }
